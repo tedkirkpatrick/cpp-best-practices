568a84f1ce2767892513ce424e0bbed1d8ec9014
Ted Kirkpatrick <ted@sfu.ca>
2022-04-08 16:33:13 -0700

    Add deliberate divide-by-0 to fuzz test
    I can't get the fuzz tester to trigger, so I'm adding this
    deliberate divide-by-0 to it.  "Deliberate" in the sense that
    over enough iterations, at least one will pass data with a string
    of four 0 bytes.

--- fuzz_test/fuzz_tester.cpp
+++ fuzz_test/fuzz_tester.cpp
@@ -1,6 +1,7 @@
+
+#include <exception>
 #include <fmt/format.h>
 #include <cstddef>
-#include <iterator>
 #include <span>
 
 #include "ops.hpp"
@@ -12,8 +13,22 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, std::size_t Size)
   auto sz = 2 * (Size / (2 * sizeof(int)));
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
   auto vals = std::span<const int> (reinterpret_cast<const int*>(Data), sz);
+  if (not vals.empty()) {
+    // cppcheck-suppress unreadVariable
+    [[maybe_unused]] auto v = 1 / vals[0];
+    /*
+    // cppcheck-suppress unreadVariable
+    [[maybe_unused]] auto v = ops::add(*vals.begin(), *(std::next(vals.begin())));
+    */
+  }
+  /*
   for (auto it = vals.begin(); it < vals.end(); it += 2) {
     fmt::print("{}\n", ops::add(*it, *(it + 1)));
+    //[[maybe_unused]] auto v = ops::add(*it, *(it + 1));
+    // cppcheck-suppress unreadVariable
+    [[maybe_unused]] auto v = ops::add(2147483647, 1); // NOLINT [cppcoreguidelines-avoid-magic-numbers,readability-magic-numbers]
+    [[maybe_unused]] auto s = Data[Size]; // NOLINT [cppcoreguidelines-pro-bounds-pointer-arithmetic]
   }
+  */
   return 0;
 }
