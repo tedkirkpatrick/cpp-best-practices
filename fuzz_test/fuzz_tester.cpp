60687e97f30674bed329e10d58c8358dcc72f5ea
Ted Kirkpatrick <ted@sfu.ca>
2022-04-07 17:55:16 -0700

    Use std::span

--- fuzz_test/fuzz_tester.cpp
+++ fuzz_test/fuzz_tester.cpp
@@ -1,5 +1,7 @@
 #include <fmt/format.h>
 #include <cstddef>
+#include <iterator>
+#include <span>
 
 #include "ops.hpp"
 
@@ -7,12 +9,11 @@
 // cppcheck-suppress unusedFunction symbolName=LLVMFuzzerTestOneInput
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *Data, std::size_t Size)
 {
-  for (std::size_t next = 0; next < Size - Size % sizeof(int); next += 2 * sizeof(int)) {
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast,cppcoreguidelines-pro-bounds-pointer-arithmetic)
-    auto lhs = * reinterpret_cast<const int*>(Data + next);
-    // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast,cppcoreguidelines-pro-bounds-pointer-arithmetic)
-    auto rhs = * reinterpret_cast<const int*>(Data + next + sizeof(int));
-    (void) ops::add(lhs, rhs);
+  auto sz = 2 * (Size / (2 * sizeof(int)));
+  // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
+  auto vals = std::span<const int> (reinterpret_cast<const int*>(Data), sz);
+  for (auto it = vals.begin(); it < vals.end(); it += 2) {
+    fmt::print("{}\n", ops::add(*it, *(it + 1)));
   }
   return 0;
 }
